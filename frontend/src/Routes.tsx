24de5ddf6c6f90f80983983100d95b5fccb94b0e
Renan Duarte <renandg.axl@gmail.com>
2022-07-06 09:03:50 -0300

    Frontend - Navbar layout

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "pages/Home";
-
+import Navbar from "components/Navbar";
 
 const Routes = () => (
   <BrowserRouter>
+    <Navbar />
     <Switch>
       <Route path="/" exact>
         <Home />
