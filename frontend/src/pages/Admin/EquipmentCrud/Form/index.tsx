4907539c359d2347dd9a6c2f17c5e4a23bf55822
Renan Duarte <renandg.axl@gmail.com>
2022-07-11 13:23:55 -0300

    Frontend - Create a new equipment

--- frontend/src/pages/Admin/EquipmentCrud/Form/index.tsx
+++ frontend/src/pages/Admin/EquipmentCrud/Form/index.tsx
@@ -1,33 +1,82 @@
-import './styles.css';
+import { AxiosRequestConfig } from "axios";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Equipment } from "types/equipment";
+import { requestBackend } from "util/request";
+import "./styles.css";
 
+type UrlParams = {
+  equipmentId: string;
+};
 
 const Form = () => {
+  const { equipmentId } = useParams<UrlParams>();
+
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Equipment>();
+
+  const onSubmit = (formData: Equipment) => {
+    const data = {
+      ...formData,
+    };
+
+    const config: AxiosRequestConfig = {
+      method: "POST",
+      url: "equipments",
+      data,
+    };
+
+    requestBackend(config)
+      .then(() => {
+        toast.info("Equipamento cadastrado com sucesso");
+        history.push("/admin/equipments");
+      })
+      .catch(() => {
+        toast.error("Erro ao cadastrar um equipamento");
+      });
+  };
+
+  const handleCancel = () => {
+    history.push("/admin/equipments");
+  };
 
   return (
     <div className="equipment-crud-container">
       <div className="base-card equipment-crud-form-card">
         <h1 className="equipment-crud-form-title">DADOS DO EQUIPAMENTO</h1>
 
-        <form >
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row equipment-crud-inputs-container">
-
             <div className="col-lg-6 equipment-crud-inputs-left-container">
-              
               <div className="margin-bottom-30">
-              <input
+                <input
+                  {...register("model", {
+                    required: "Campo obrigatório",
+                  })}
                   type="text"
-                  className="form-control base-input" 
+                  className={`form-control base-input ${
+                    errors.model ? "is-invalid" : ""
+                  }`}
                   placeholder="Modelo do equipamento"
                   name="model"
                 />
+                <div className="invalid-feedback d-block">
+                  {errors.model?.message}
+                </div>
               </div>
-
-          </div>
-
+            </div>
           </div>
 
           <div className="equipment-crud-buttons-container">
             <button
+              onClick={handleCancel}
               className="btn btn-outline-danger equipment-crud-button"
             >
               CANCELAR
