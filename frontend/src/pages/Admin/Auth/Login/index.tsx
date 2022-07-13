a1722108407dfbd9a18ce464e4bb12174c64edc9
Renan Duarte <renandg.axl@gmail.com>
2022-07-12 21:29:04 -0300

    Frontend - Request the Token and login

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -1,36 +1,99 @@
-import ButtonIcon from 'components/ButtonIcon';
+import ButtonIcon from "components/ButtonIcon";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory, useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
+import { requestBackendLogin } from "util/request";
 
-import './styles.css';
+import "./styles.css";
+
+type CredentialsDTO = {
+  username: string;
+  password: string;
+};
+
+type LocationState = {
+  from: string;
+};
 
 const Login = () => {
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: "/" } };
+
+  const [hasError, setHasError] = useState(false);
+
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CredentialsDTO>();
+
+  const onSubmit = (formData: CredentialsDTO) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        console.log(response.data);
+        setHasError(false);
+        history.replace(from);
+        toast.info("Usuário Logado com sucesso");
+      })
+      .catch((error) => {
+        setHasError(true);
+        toast.error("Erro ao tentar logar");
+      });
+  };
+
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      
-      <form>
-
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
+            {...register("username", {
+              required: "Campo obrigatório",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Email inválido",
+              },
+            })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${
+              errors.username ? "is-invalid" : ""
+            }`}
             placeholder="Email"
             name="username"
           />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
 
         <div className="mb-2">
           <input
+            {...register("password", {
+              required: "Campo obrigatório",
+            })}
             type="password"
-            className="form-control base-input"
+            className={`form-control base-input ${
+              errors.password ? "is-invalid" : ""
+            }`}
             placeholder="Password"
             name="password"
           />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
-        <br/>
-        <br/>
+        <br />
+        <br />
 
         <div className="login-submit">
-          <ButtonIcon text="Logar"/>
+          <ButtonIcon text="Logar" />
         </div>
       </form>
     </div>
@@ -38,4 +101,3 @@ const Login = () => {
 };
 
 export default Login;
-
